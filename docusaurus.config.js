590079453e0b906f3f03e4369d03c33526860414
bsuth <bsuth701@gmail.com>
2023-09-03 21:25:41 +0900

    update baseUrl to match domain name change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ module.exports = {
   organizationName: 'bsuth',
   projectName: 'punchfolio',
   url: 'https://punchfolio.com',
-  baseUrl: process.env.NODE_ENV === 'production' ? '/punchfolio/' : '/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
